abc0d5b1baba08cb527bebdd7d70a3af580bc9e4
Belove <beloveolusola@gmail.com>
2024-01-12 03:42:01 +0100

    Encryption

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -44,12 +44,11 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     Creates a connector to a database
     """
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     connection = mysql.connector.connect(
         host=db_host,
-        port=3306,
         user=db_user,
         password=db_pwd,
         database=db_name,
