0ea9fdb5c9c8a3490a5775b62697e11e534e7cef
Belove <beloveolusola@gmail.com>
2024-01-26 01:39:50 +0100

    Update user details

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -62,3 +62,22 @@ class DB:
         if result is None:
             raise NoResultFound()
         return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update user info
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update,
+            synchronize_session=False,
+        )
+        self._session.commit()
